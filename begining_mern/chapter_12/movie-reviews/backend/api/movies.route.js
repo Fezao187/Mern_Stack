13a90fd811e04435671cd367b57a6b8d3a4ac5ee
Katlego Maredi <katlegomaredi9@gmail.com>
2024-06-06 10:48:28 +0200

    Chapter 12: Route to get a single movie and its ratings

--- begining_mern/chapter_12/movie-reviews/backend/api/movies.route.js
+++ begining_mern/chapter_12/movie-reviews/backend/api/movies.route.js
@@ -7,5 +7,7 @@ router
     .route("/review")
     .post(ReviewsController.apiPostReview)
     .put(ReviewsController.apiUpdateReview)
-    .delete(ReviewsController.apiDeleteReview)
+    .delete(ReviewsController.apiDeleteReview);
+router.route("/id/:id").get(MoviesController.apiGetMovieById)
+router.route("/ratings").get(MoviesController.apiGetRatings)
 export default router;
--- begining_mern/chapter_12/movie-reviews/backend/dao/reviewsDAO.js
+++ begining_mern/chapter_12/movie-reviews/backend/dao/reviewsDAO.js
@@ -33,7 +33,7 @@ export default class ReviewsDAO {
     static async updateReview(reviewId, userId, review, date) {
         try {
             const updateResponse = await reviews.updateOne(
-                { user_id: userId, _id: ObjectId(reviewId) },
+                { user_id: userId, _id: new ObjectId(reviewId) },
                 { $set: { review: review, date: date } }
             )
             return updateResponse;
@@ -46,7 +46,7 @@ export default class ReviewsDAO {
     static async deleteReview(reviewId, userId) {
         try {
             const deleteResponse = await reviews.deleteOne({
-                _id: ObjectId(reviewId),
+                _id: new ObjectId(reviewId),
                 user_id: userId,
             });
             return deleteResponse;
