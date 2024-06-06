7c2bff4c56d8dc4dc614607c0a7db427e4f2af2e
Katlego Maredi <katlegomaredi9@gmail.com>
2024-06-06 11:34:10 +0200

    Chapter 13: Introduction to react

---
+++ begining_mern/chapter_13/movie-reviews/frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+
+export default App;
