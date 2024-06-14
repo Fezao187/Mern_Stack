b249dea230402c3150a65c3c1ee96ad746854577
Katlego Maredi <katlegomaredi9@gmail.com>
2024-06-14 10:38:03 +0200

    Update login

--- begining_mern/chapter_20/movie-reviews/frontend/src/components/login.js
+++ begining_mern/chapter_20/movie-reviews/frontend/src/components/login.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 const Login = props => {
-    const [name, setName] = useState("")
-    const [id, setId] = useState("")
+    const [name, setName] = useState("");
+    const [id, setId] = useState("");
     const onChangeName = e => {
         const name = e.target.value
         setName(name);
