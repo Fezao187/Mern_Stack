5480efb02b9d279d8f9ca60ea101d0af8e985a4d
Katlego Maredi <katlegomaredi9@gmail.com>
2024-06-14 16:04:02 +0200

    Save new book with mongoose

--- turorials/tut1_bookstore/backend/models/bookModels.js
+++ turorials/tut1_bookstore/backend/models/bookModels.js
@@ -2,20 +2,21 @@ import mongoose from "mongoose";
 
 const bookSchema = mongoose.Schema(
     {
-        title:{
-            type:String,
-            required:true
+        title: {
+            type: String,
+            required: true
         },
-        author:{
-            type:String,
-            required:true
+        author: {
+            type: String,
+            required: true
         },
-        publisher:{
-            type:Number,
-            required:true
+        publishYear: {
+            type: Number,
+            required: true
         }
-    },{
-        timestamps:true
+    },
+    {
+        timestamps: true
     }
 );
-export const Book = mongoose.model("Book",bookSchema);
+export const Book = mongoose.model("Book", bookSchema);
