635f4b3c39fa7ac40c44305f3ec215bf89711b13
Katlego Maredi <katlegomaredi9@gmail.com>
2024-07-04 13:35:48 +0200

    Generate JWT

--- turorials/tut2_event_booking/backend/graphql/resolvers/auth.js
+++ turorials/tut2_event_booking/backend/graphql/resolvers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
     createUser: async args => {
@@ -24,13 +25,25 @@ module.exports = {
         };
     },
     login: async ({ email, password }) => {
-        const user = User.findOne({ email: email });
+        const user = await User.findOne({ email: email });
         if (!user) {
             throw new Error("User does not exist");
         }
         const isEqual = await bcrypt.compare(password, user.password);
         if (!isEqual) {
             throw new Error("Password is incorrect");
+        }
+        const token = jwt.sign({
+            userId: user.id,
+            email: user.email
+        }, "WeirdKey",
+            {
+                expiresIn: "1h"
+            });
+        return {
+            userId: user.id,
+            token: token,
+            tokenExpiration: 1
         }
     }
 }
