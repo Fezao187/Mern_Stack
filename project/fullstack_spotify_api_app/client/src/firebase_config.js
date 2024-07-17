5f93e0607fe037f3b1703fec8b14a756155a0237
Fezao187 <katlegomaredi9@gmail.com>
2024-07-18 00:26:01 +0200

    Hiding frontend secrets

---
+++ project/fullstack_spotify_api_app/client/src/firebase_config.js
@@ -0,0 +1,29 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import dotenv from 'dotenv';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+dotenv.config();
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.GOOGLE_API_KEY,
+    authDomain: "spotify-api-app-4e4c4.firebaseapp.com",
+    projectId: "spotify-api-app-4e4c4",
+    storageBucket: "spotify-api-app-4e4c4.appspot.com",
+    messagingSenderId: "997138643364",
+    appId: "1:997138643364:web:5968f4d860000c90bb6931",
+    measurementId: "G-PMBBRE20YJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+export default app;
