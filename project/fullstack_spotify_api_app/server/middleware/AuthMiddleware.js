77ee439095d6d3877db488edc681d0892952040d
Fezao187 <katlegomaredi9@gmail.com>
2024-07-23 10:07:01 +0200

    Add middleware to secure routes

---
+++ project/fullstack_spotify_api_app/server/middleware/AuthMiddleware.js
@@ -0,0 +1,30 @@
+import { User } from "../models/User";
+import { config } from "dotenv";
+import jwt from "jsonwebtoken";
+
+config();
+// Check if user has access to the route
+export const userVerification = (req, res) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.json({ status: false })
+    }
+    // Check if tokens match
+    jwt.verify(token,
+        process.env.TOKEN_KEY,
+        async (err, data) => {
+            if (err) {
+                return res.json({ status: false })
+            } else {
+                const user = await User.findById(data.id);
+                if (user) {
+                    return res.json({
+                        status: true,
+                        user: user.username
+                    });
+                } else {
+                    return res.json({ status: false });
+                }
+            }
+        });
+}
