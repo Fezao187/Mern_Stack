9e69bf68bf6fba6d0004c1a2f3473bee816239f4
Fezao187 <katlegomaredi9@gmail.com>
2024-07-23 09:31:24 +0200

    Create login function to authenticate user

--- project/fullstack_spotify_api_app/server/controllers/AuthController.js
+++ project/fullstack_spotify_api_app/server/controllers/AuthController.js
@@ -29,3 +29,37 @@ export const Signup = async (req, res, next) => {
         console.log(error);
     }
 }
+
+export const Login = async (req, res, next) => {
+    try {
+        // Get all variables from the body
+        const { email, password } = req.body;
+        // Check if email exists on DB
+        if (!email || !password) {
+            return res.json({ message: "All fields are required" })
+        }
+        // Check if email exists in DB
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.json({ message: "Incorrect password or email" })
+        }
+        // Check if req.body is the same password as the on in DB
+        const auth = await bcrypt.compare(password, user.password);
+        if (!auth) {
+            return res.json({ message: "Password or email is incorrect" })
+        }
+        const token = createSecretToken(user._id);
+        res.cookie("token", token, {
+            withCredentials: true,
+            httpOnly: false,
+        });
+        res.status(201)
+            .json({
+                message: "User logged in successfully",
+                success: true
+            });
+        next();
+    } catch (error) {
+        console.error(error);
+    }
+}
