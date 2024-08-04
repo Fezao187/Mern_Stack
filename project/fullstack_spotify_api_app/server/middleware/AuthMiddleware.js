9adc322db4ce3a5a018230e33cdf04a9551c679c
Fezao187 <katlegomaredi9@gmail.com>
2024-08-04 13:34:24 +0200

    Auth middleware

--- project/fullstack_spotify_api_app/server/middleware/AuthMiddleware.js
+++ project/fullstack_spotify_api_app/server/middleware/AuthMiddleware.js
@@ -4,38 +4,59 @@ import jwt from "jsonwebtoken";
 
 config();
 // Check if user has access to the route
-export const userVerification = (req, res, next) => {
-    const token = req.cookies.token;
-    if (!token) {
-        return res.json({
-            status: false,
-            message: "Token not found"
-        });
+export const userVerification = async (req, res, next) => {
+    // const token = req.cookies.token;
+    // if (!token) {
+    //     return res.json({
+    //         status: false,
+    //         message: "Token not found"
+    //     });
+    // }
+    // // Check if tokens match
+    // jwt.verify(token,
+    //     process.env.TOKEN_KEY,
+    //     async (err, data) => {
+    //         if (err) {
+    //             return res.json({
+    //                 status: false,
+    //                 message: err
+    //             });
+    //         } else {
+    //             const user = await User.findById(data.id);
+    //             if (user) {
+    //                 req.user = user;
+    //                 next();
+    //                 return res.json({
+    //                     status: true,
+    //                     user: user.username
+    //                 })
+    //             } else {
+    //                 return res.json({
+    //                     status: false,
+    //                     message: "Tokens don't match"
+    //                 });
+    //             }
+    //         }
+    //     });
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        try {
+            // Get token from header
+            token = req.headers.authorization.split(" ")[1];
+            // Verify token
+            const decoded = jwt.verify(token, process.env.TOKEN_KEY)
+            // Get user from token
+            req.user = await User.findById(decoded.id).select("-password");
+            next();
+        } catch (error) {
+            console.log(error);
+            res.status(401)
+                .json({ message: "Not authorized" });
+        }
     }
-    // Check if tokens match
-    jwt.verify(token,
-        process.env.TOKEN_KEY,
-        async (err, data) => {
-            if (err) {
-                return res.json({
-                    status: false,
-                    message: err
-                });
-            } else {
-                const user = await User.findById(data.id);
-                if (user) {
-                    req.user = user;
-                    next();
-                    return res.json({
-                        status: true,
-                        user: user.username
-                    })
-                } else {
-                    return res.json({
-                        status: false,
-                        message: "Tokens don't match"
-                    });
-                }
-            }
-        });
+    if (!token) {
+        return res.status(401)
+            .json({ message: "Not authorized, Token not found" });
+    }
 }
